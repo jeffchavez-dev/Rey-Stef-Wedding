acff77fccf32d4a031f02e6878fbb856e7bafe5a
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-11-21 12:33:35 +0800

    Update

--- script.js
+++ script.js
@@ -3,20 +3,22 @@ const countdownElement = document.getElementById('countdown');
 const countdownDate = new Date('November 5, 2024 16:00:00').getTime();
 
 const intervalId = setInterval(() => {
-    const now = new Date().getTime();
-    const distance = countdownDate - now;
-
+  const now = new Date().getTime();
+  
+  const distance = countdownDate - now;
+  alert(distance)
+  
+  if (distance < 0) {
+    clearInterval(intervalId);
+    countdownElement.innerHTML = 'The wedding is today!';
+  } else {
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     countdownElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-
-    if (distance < 0) {
-        clearInterval(intervalId);
-        countdownElement.innerHTML = 'The wedding is today!';
-    }
+  }
 }, 1000);
 
 intervalId()
